26ac42349c0e27c619d1aa5e0005331130fcb8bb
AkashAmrolkar <akashamrolkar10@gmail.com>
2024-06-26 22:20:55 +0530

    Updated about info

--- client/src/component/About.jsx
+++ client/src/component/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <div className="grid grid-cols-12 items-center gap-5">
           <div className="col-span-12 md:col-span-6">
             <h2 className=" text-4xl font-bold mb-8 text-primary title_bfr_efc relative pl-14">About Me</h2>
-            <div className="text-white text-lg mb-8">Hi! I'm Akash Amrolkar, a passionate Full Stack Developer with a love for creating dynamic, responsive, and user-friendly web applications. With a solid foundation in both front-end and back-end technologies, I enjoy transforming complex problems into intuitive, accessible, and efficient solutions.</div>
+            <div className="text-white text-lg mb-8">Highly skilled Full Stack Developer with 3 years of experience in designing, developing, and maintaining web applications. Proficient in front-end technologies including HTML, CSS, Bootstrap, JavaScript, React, Next.js, and Redux, and back-end technologies like PHP, Node.js, and Express.js. Experienced in database management with MongoDB and MySQL, and adept at using CMS platforms such as WordPress and Shopify. Strong in RESTful API development, responsive web design, and version control with Git. Proven track record of optimizing application performance, debugging, and troubleshooting. Effective communicator and collaborator with a strong problem-solving aptitude.</div>
             <div className="flex flex-wrap gap-5 items-center mb-8">
               <Link to='#contact' className="rounded-md bg-primary px-5 py-3 text-secondary font-semibold hover:text-secondary">Hire Me</Link>
               <a href={resume} rel='noreferrer' target='_blank' className=' rounded-md bg-primary px-5 py-3 w-fit text-secondary font-semibold'>Download Resume</a>
